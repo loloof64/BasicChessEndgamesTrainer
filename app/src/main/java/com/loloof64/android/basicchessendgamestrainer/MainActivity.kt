38fad2c99d09247f3b1a5958e84e204f1db92beb
loloof64 <laurent.bernabe@gmail.com>
2022-07-16 10:26:14 +0200

    Using the builder pattern for parameters for the chessboard composable.

--- app/src/main/java/com/loloof64/android/basicchessendgamestrainer/MainActivity.kt
+++ app/src/main/java/com/loloof64/android/basicchessendgamestrainer/MainActivity.kt
@@ -3,16 +3,23 @@ package com.loloof64.android.basicchessendgamestrainer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.loloof64.android.basicchessendgamestrainer.ui.theme.BasicChessEndgamesTheme
 import com.loloof64.android.chessboard.ui.ChessBoard
+import com.loloof64.android.chessboard.ui.ChessBoardParameters
+import com.loloof64.android.chessboard.ui.ChessBoardParametersBuilder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,5 +54,16 @@ fun DefaultPreview() {
 @Preview
 @Composable
 fun ChessBoardPreview() {
-    ChessBoard(300.dp)
+    val boardParameters = ChessBoardParametersBuilder()
+        .setBackgroundColorTo(Color.Yellow)
+        .setTotalSizeTo(300.dp)
+        .build()
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ChessBoard(parameters = boardParameters)
+    }
 }
--- chessboard/src/main/java/com/loloof64/android/chessboard/ui/ChessBoard.kt
+++ chessboard/src/main/java/com/loloof64/android/chessboard/ui/ChessBoard.kt
@@ -9,12 +9,42 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+class ChessBoardParametersBuilder
+{
+    var totalSize: Dp = 100.dp
+    var backgroundColor = Color(0xFFBF360C)
+
+    fun setTotalSizeTo(size: Dp) : ChessBoardParametersBuilder {
+        totalSize = size
+        return this
+    }
+
+    fun setBackgroundColorTo(color: Color): ChessBoardParametersBuilder {
+        backgroundColor = color
+        return this
+    }
+
+    fun build(): ChessBoardParameters {
+        return ChessBoardParameters(
+            backgroundColor = backgroundColor,
+            totalSize = totalSize
+        )
+    }
+}
+
+data class ChessBoardParameters(
+    val backgroundColor: Color,
+    val totalSize: Dp
+)
+
 @Composable
-fun ChessBoard(totalSize: Dp) {
-    val backgroundColor = Color(0xFFBF360C)
-    Surface(modifier = Modifier
-        .requiredSize(totalSize),
-        color = backgroundColor
+fun ChessBoard(modifier: Modifier = Modifier,
+        parameters: ChessBoardParameters
+               ) {
+
+    Surface(modifier = modifier
+        .requiredSize(parameters.totalSize),
+        color = parameters.backgroundColor
     ) {
 
     }
